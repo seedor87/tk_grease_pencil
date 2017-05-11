46abe95500a0ea1b034664c257a34fd9d4be5e22
Bob <seedor87@students.rowan.edu>
2017-05-11 14:01:58 -0400

    added rough free hand drawing tool, TODO debug the problems in the canvas image update

--- canvas_tool.py
+++ canvas_tool.py
@@ -1,14 +1,20 @@
 from Tkinter import *
+import tkFileDialog
+from tkinter.colorchooser import *
 from PIL import ImageTk, ImageGrab, Image
+import os
 
 # TOOLS
-LINE, RECTANGLE, CIRCLE, TEXT, GRID, UNDO, SAVE = list(range(7))
+LINE, RECTANGLE, CIRCLE, TEXT, FREE, GRID, UNDO, SAVE, COLOR = list(range(9))
 
 # path to background image
-path = '/Users/robertseedorf/PycharmProjects/grease_pencil/Cover-1422383435.jpg'
+path = os.path.join('.', 'MTRAINIER.jpg')
+# path = os.path.join('.', 'MTDENALI.jpg')
 
 # history stack
 HISTORY = list()
+
+root = Tk()
 
 class popupWindow(object):
     def __init__(self,master):
@@ -32,29 +38,75 @@ class Paint:
         self.canvas.bind('<Button-1>', self.update_xy)
         self.canvas.bind('<B1-Motion>', self.draw)
 
+        self.canvas_image = None
+        self.FILL = 'black'
+
+        self.file_opt = options = {}
+        options['defaultextension'] = '.jpg'
+        options['filetypes'] = [('all files', '.*'), ('text files', '.txt'), ('jpg files', '*.jpg')]
+        options['initialdir'] = 'C:\\Users\\Bob S\\PycharmProjects\\tk_grease_pencil'
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = root
+        options['title'] = 'This is a title'
+
+        menubar = Menu(root)
+
+        # create a pulldown menu, and add it to the menu bar
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open", command=self.set_background)
+        filemenu.add_command(label="Save", command=hello)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=root.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        # create more pulldown menus
+        editmenu = Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Cut", command=hello)
+        editmenu.add_command(label="Copy", command=hello)
+        editmenu.add_command(label="Paste", command=hello)
+        menubar.add_cascade(label="Edit", menu=editmenu)
+
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="About", command=hello)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+        # display the menu
+        root.config(menu=menubar)
 
     def draw(self, event):
         if self._tool is None or self._obj is None:
             return
         x, y = self.lastx, self.lasty
-        if self._tool in (LINE, RECTANGLE, CIRCLE, TEXT, GRID, UNDO, SAVE):
+        if self._tool in (LINE, RECTANGLE, CIRCLE, TEXT, GRID, UNDO, SAVE, COLOR):
             self.canvas.coords(self._obj, (x, y, event.x, event.y))
+        else:
+            x1, y1 = (event.x - 1), (event.y - 1)
+            x2, y2 = (event.x + 1), (event.y + 1)
+            print x1, x2, ' -- ', y1, y2
+            self._obj = self.canvas.create_oval(x1, y1, x2, y2, fill="red")
+            HISTORY.append(self._obj)
+
+    def set_background(self):
+        path = tkFileDialog.askopenfilename(**self.file_opt)
+        img = Image.open(path)
+        img = ImageTk.PhotoImage(img)
+        self.canvas.itemconfig(self.canvas, image=img)
 
     def update_xy(self, event):
         if self._tool is None:
             return
         x, y = event.x, event.y
         if self._tool == LINE:
-            self._obj = self.canvas.create_line((x, y, x, y))
+            self._obj = self.canvas.create_line((x, y, x, y), fill=self.FILL)
             HISTORY.append(self._obj)
         if self._tool == RECTANGLE:
-            self._obj = self.canvas.create_rectangle((x, y, x, y))
+            self._obj = self.canvas.create_rectangle((x, y, x, y), fill=self.FILL)
             HISTORY.append(self._obj)
         if self._tool == CIRCLE:
-            self._obj = self.canvas.create_oval((x, y, x, y))
+            self._obj = self.canvas.create_oval((x, y, x, y), fill=self.FILL)
             HISTORY.append(self._obj)
         if self._tool == TEXT:
-            self._obj = self.canvas.create_text(x, y)
+            self._obj = self.canvas.create_text(x, y, fill=self.FILL)
             self.w = popupWindow(self.canvas)
             self.canvas.wait_window(self.w.top)
             self.canvas.itemconfig(self._obj, text=self.w.value)
@@ -65,7 +117,8 @@ class Paint:
                 self.canvas.create_line(0, 50 * i, 400, 50 * i)
         if self._tool == UNDO:
             try:
-                self.canvas.delete(HISTORY.pop())
+                act = HISTORY.pop()
+                self.canvas.delete(act)
             except IndexError as e:
                 print e
         elif self._tool == SAVE:
@@ -74,6 +127,7 @@ class Paint:
             x1=x+self.canvas.winfo_width()
             y1=y+self.canvas.winfo_height()
             ImageGrab.grab().crop((x,y,x1,y1)).save("out.png")
+
         self.lastx, self.lasty = x, y
 
 
@@ -87,8 +141,8 @@ class Tool:
 
         frame = Frame(parent)
         self._curr_tool = None
-        for i, (text, t) in enumerate((('L', LINE), ('R', RECTANGLE), ('C', CIRCLE), ('T', TEXT), ('G', GRID), ('U', UNDO), ('S', SAVE))):
-            lbl = Label(frame, text=text, width=2, relief='raised')
+        for i, (text, t) in enumerate((('Line', LINE), ('Rect', RECTANGLE), ('Circ', CIRCLE), ('Text', TEXT), ('Free', FREE), ('Grid', GRID), ('Undo', UNDO), ('SAVE', SAVE), ('Color', COLOR))):
+            lbl = Label(frame, text=text, width=5, relief='raised')
             lbl._tool = t
             lbl.bind('<Button-1>', self.update_tool)
             lbl.pack(padx=6, pady=6*((i+1) % 2), anchor='nw')
@@ -105,7 +159,6 @@ class Tool:
 def hello():
     print 'hello'
 
-root = Tk()
 
 canvas = Canvas(highlightbackground='black')
 whiteboard = Paint(canvas)
@@ -113,32 +166,8 @@ tool = Tool(whiteboard)
 canvas.pack(fill='both', expand=True, padx=6, pady=6, anchor='w')
 img = Image.open(path)
 img = ImageTk.PhotoImage(img)
-canvas.create_image((0, 0), image=img, anchor="nw")
+Paint.canvas_image = canvas.create_image((0, 0), image=img, anchor="nw")
 w, h = img.width(), img.height()
 canvas.config(width=w, height=h)
-
-menubar = Menu(root)
-
-# create a pulldown menu, and add it to the menu bar
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=hello)
-filemenu.add_command(label="Save", command=hello)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-# create more pulldown menus
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Cut", command=hello)
-editmenu.add_command(label="Copy", command=hello)
-editmenu.add_command(label="Paste", command=hello)
-menubar.add_cascade(label="Edit", menu=editmenu)
-
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="About", command=hello)
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-# display the menu
-root.config(menu=menubar)
 
 root.mainloop()
--- tb.py
+++ tb.py
@@ -1,44 +1,23 @@
-class my_obj():
+from Tkinter import *
 
-    def __init__(self, val):
-        self.val = val
+class PaintBox( Frame ):
+   def __init__( self ):
+      Frame.__init__( self )
+      self.pack( expand = YES, fill = BOTH )
+      self.master.title( "title" )
+      self.master.geometry( "300x150" )
 
-    def __repr__(self):
-        return str(self.val)
+      self.message = Label( self, text = "Drag the mouse to draw" )
+      self.message.pack( side = BOTTOM )
 
-class my_sup():
+      self.myCanvas = Canvas( self )
+      self.myCanvas.pack( expand = YES, fill = BOTH )
 
-    def __init__(self):
-        self.val = None
+      self.myCanvas.bind( "<B1-Motion>", self.paint )
 
-    def exe(self):
-        lis = list()
-        self.val = my_obj(3)
-        lis.append(self.val)
-        print lis
-        self.val = my_obj(5)
-        lis.append(self.val)
-        print lis
-        self.val = my_obj(7)
-        lis.append(self.val)
-        print lis
+   def paint( self, event ):
+      x1, y1 = ( event.x - 4 ), ( event.y - 4 )
+      x2, y2 = ( event.x + 4 ), ( event.y + 4 )
+      self.myCanvas.create_oval( x1, y1, x2, y2, fill = "red" )
 
-        lis.pop()
-        print lis
-
-        self.val=my_obj(9)
-        lis.append(self.val)
-        print lis
-
-        lis.pop()
-        lis.pop()
-        lis.pop()
-        print lis
-
-        try:
-            lis.pop()
-        except IndexError:
-            pass
-
-m = my_sup()
-m.exe()
+PaintBox().mainloop()
